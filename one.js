d74afc09610fe62f75b3e8b72609ca7bd5fba7b5
Bhagyasree <tellibhagyasree17@gmail.com>
2025-06-12 06:50:07 +0000

    i have learnt functions

---
+++ one.js
@@ -0,0 +1,52 @@
+//functions are one of the powerful features in javascript.
+//they allow you to reuse code, orgnise logic,and handle dynamic inputs.
+//declaration + call
+//syntax
+//function declaration
+sayHello()
+sayHello()
+function sayHello() {
+    console.log("hello")
+}
+
+//functon calling
+
+
+
+// console.log(a)
+let a = 10;
+console.log(a)
+
+
+// functions in parameters
+
+function sum(){
+console.log(10 + 5)
+}
+sum()
+
+function sum(num1, num2 , num3){
+console.log(num1+ num2 + num3)
+}
+sum(10, 5, 5)
+sum(10, 10, 10)
+
+function subtraction(a, b){
+    console.log(a - b)
+}
+subtraction(10, 5)
+subtraction()
+
+function multiply(a, b){
+    console.log(a * b)
+}
+multiply(10, 5)
+multiply()
+
+
+function div(a, b){
+    console.log(a / b)
+}
+div(10, 5)
+div()
+
